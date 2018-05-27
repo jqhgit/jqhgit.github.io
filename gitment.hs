d4ae4b9716d76fa4131fcb4ce15487b794f8ab2a
jqhgit <39646985+jqhgit@users.noreply.github.com>
2018-05-27 20:26:39 +0800

    Update and rename gitment.html to gitment.hs

---
+++ gitment.hs
@@ -0,0 +1,12 @@
+
+var _gitment = new Gitment(
+{ 
+  owner: 'jqhgit',
+  repo: 'issues',
+  oauth: 
+  {
+    client_id: '7b17661d6fd03752e84a',
+    client_secret: 'b535dc46d7e98458b7e372f5e3a6258846b845c4',
+  },
+});
+_gitment.render('gitmentContainer');
